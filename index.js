f52e1d1686586274d0efcc1cb7981a4ad7b4ae54
jbassoRMIT <s4101667@student.rmit.edu.au>
2025-11-24 08:14:18 +1100

    fetch function can now take a city as input

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const weatherResults=document.querySelector(".weatherResults");
 const key="Q4ZEAHEY26Q25F7A4ZP6YLT6H"
 
 //write function to  fetch weather data and display to console
-const fetchWeather= async function(){
-    const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/melbourne/today?unitGroup=metric&include=days&key=Q4ZEAHEY26Q25F7A4ZP6YLT6H&contentType=json");
+const fetchWeather= async function(city){
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/today?unitGroup=metric&include=days&key=Q4ZEAHEY26Q25F7A4ZP6YLT6H&contentType=json`);
     const results=await response.json();
     console.log(results);
-    const address=results.address;
+    const address=city;
     const conditions=results.days[0].conditions;
     const minTemp=results.days[0].tempmin;
     const maxTemp=results.days[0].tempmax;
@@ -15,4 +15,4 @@ const fetchWeather= async function(){
     weatherResults.textContent=`The weather today in ${address} is ${conditions}. Min temp: ${minTemp}, Max temp: ${maxTemp}`;
 }
 
-fetchWeather();
+fetchWeather("Toronto");
