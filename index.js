d38a9503ff457b8a49afa84ff81decdd6b34b12a
jbassoRMIT <s4101667@student.rmit.edu.au>
2025-11-24 08:09:01 +1100

    Fetch request to weather API successful and shoprt message appears on browser with some relevant data

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("weather app");
+const weatherResults=document.querySelector(".weatherResults");
+
+const key="Q4ZEAHEY26Q25F7A4ZP6YLT6H"
+
+//write function to  fetch weather data and display to console
+const fetchWeather= async function(){
+    const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/melbourne/today?unitGroup=metric&include=days&key=Q4ZEAHEY26Q25F7A4ZP6YLT6H&contentType=json");
+    const results=await response.json();
+    console.log(results);
+    const address=results.address;
+    const conditions=results.days[0].conditions;
+    const minTemp=results.days[0].tempmin;
+    const maxTemp=results.days[0].tempmax;
+
+    weatherResults.textContent=`The weather today in ${address} is ${conditions}. Min temp: ${minTemp}, Max temp: ${maxTemp}`;
+}
+
+fetchWeather();
