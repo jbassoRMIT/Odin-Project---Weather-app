d6a41a404288eb8fc5038872998d3c02288337c5
jbassoRMIT <s4101667@student.rmit.edu.au>
2025-11-24 09:07:38 +1100

    border color formatted conditionally based on temp data presented as a list

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const citySearch=document.querySelector("#city");
 
 const key="Q4ZEAHEY26Q25F7A4ZP6YLT6H"
 
+//Write function to remove child nodes from parent
+const removeChilden=function(parent){
+        while (parent.firstChild){
+            parent.removeChild(parent.firstChild);
+        }
+}
+
 //write function to  fetch weather data and display to console
 const fetchWeather= async function(city){
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/today?unitGroup=metric&include=days&key=Q4ZEAHEY26Q25F7A4ZP6YLT6H&contentType=json`);
@@ -11,10 +18,42 @@ const fetchWeather= async function(city){
     console.log(results);
     const address=city;
     const conditions=results.days[0].conditions;
+    const temp=results.days[0].temp;
     const minTemp=results.days[0].tempmin;
     const maxTemp=results.days[0].tempmax;
+    const icon=results.days[0].icon;
 
-    weatherResults.textContent=`The weather today in ${address} is ${conditions}. Min temp: ${minTemp}, Max temp: ${maxTemp}`;
+    if(temp<10){
+        weatherResults.className="weatherResults cold";
+    }
+    else if(temp<20){
+        weatherResults.className="weatherResults mild";
+    } else{
+        weatherResults.className="weatherResults warm";
+    }
+
+    //clear contents of weatherDisplay
+    removeChilden(weatherResults);
+
+    //display data as a list
+    const listHeader=document.createElement("p");
+    listHeader.textContent=`The weather today in ${address} is:`;
+    const list=document.createElement("ul");
+    const conditionList=document.createElement("li");
+    conditionList.textContent=`Conditions: ${conditions}`;
+    const tempList=document.createElement("li");
+    tempList.textContent=`Temp: ${temp}`;
+    const minTempList=document.createElement("li");
+    minTempList.textContent=`Min temp: ${minTemp}`;
+    const maxTempList=document.createElement("li");
+    maxTempList.textContent=`Max temp: ${maxTemp}`;
+
+    weatherResults.appendChild(listHeader);
+    weatherResults.appendChild(list);
+    list.appendChild(conditionList);
+    list.appendChild(tempList);
+    list.appendChild(minTempList);
+    list.appendChild(maxTempList);
 }
 
 //add event listener to form
